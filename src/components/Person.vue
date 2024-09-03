062e1bbd326b31e2075c082303c2e20182ba1d36
thomas <thomashuan88@gmail.com>
2024-09-03 16:50:00 +0000

    setup extend

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -8,35 +8,25 @@
     </div>
 </template>
 
-<script lang="ts">
-export default {
-    name: 'Person',
-    setup() {
-        let name = 'bob'
-        let age = 20
-        let tel = '123333333'
+<script lang="ts" setup name="Person123">
+    let name = 'bob'
+    let age = 20
+    let tel = '123333333'
 
-        //methods
-        function chgName() {
-            console.log('chgName')
-            name = 'jack' // like this , page not re-render
-        }
-
-        function chgAge() {
-            console.log('chgAge')
-            age += 1
-        }
-
-        function showContact() {
-            console.log('showContact')
-            alert(tel)
-        }
-
-        return {name, age, chgName, chgAge, showContact} // if not return , page not re-render
-        // return () => 'hello' // return can be a render function
-
+    function chgName() {
+        console.log('chgName')
+        name = 'jack' // like this , page not re-render
     }
-}
+
+    function chgAge() {
+        console.log('chgAge')
+        age += 1
+    }
+
+    function showContact() {
+        console.log('showContact')
+        alert(tel)
+    }
 </script>
 
 <style scoped>
