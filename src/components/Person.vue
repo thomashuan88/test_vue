9626ef744ea6896e79f9aaebb17e61a797e0d071
thomas <thomashuan88@gmail.com>
2024-09-04 06:29:17 +0000

    watch ref

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,31 +1,47 @@
 <template>
     <div class="person">
-        <h2>sum: {{ sum }}</h2>
-        <button @click="chgSum">plus one</button>
+        <h1>scene 2: monitor object data type</h1>
+        <h2>name: {{ person.name }}</h2>
+        <h2>age: {{ person.age }}</h2>
+        <button @click="chgName">change name</button>
+        <button @click="chgAge">change age</button>
+        <button @click="chgPerson">change person</button>
     </div>
 </template>
 
 <script lang="ts" setup name="Person">
     import {ref, watch} from 'vue'
 
-    let sum = ref(0)
+    let person = ref({
+        name: 'bob',
+        age: 20
+    })
 
-    function chgSum() {
-        sum.value += 1
+    function chgName() {
+        person.value.name += '~'
     }
 
-    let i = 0
+    function chgAge() {
+        person.value.age += 1
+    }
 
-    const stopWatch = watch(sum, (newValue, oldValue) => {
+    function chgPerson() {
+        person.value = {
+            name: 'jack',
+            age: 30
+        }
+    }
+
+    // like this cannot monitor nested object (name, age) , need to manually open deep watch
+    // watch(person, (newValue, oldValue) => {
+    //     console.log(newValue, oldValue)
+    // })
+
+    watch(person, (newValue, oldValue) => {
         console.log(newValue, oldValue)
-        i += 1
-        if (newValue > 10) {
-            sum.value = 0
-        }
-        if (i > 20) {
-            stopWatch()
-        }
-        console.log(i)
+    }, {
+        deep: true
+        // immediate: true // default // start from page start
     })
 </script>
 
