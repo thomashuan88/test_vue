f96301dc5edd3d19380dfdf506b4fb906dcf660a
thomas <thomashuan88@gmail.com>
2024-09-04 03:09:15 +0000

    with ref

---
+++ 05_reactive&ref/components/Person.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="person">
+        <h2>A {{ car.brand }} car, price is {{ car.price }}</h2>
+        <button @click="chgPrice">change car price</button>
+        <button @click="chgCar">change car</button>
+        <hr>
+        <h2>sum: {{ sum }}</h2>
+        <button @click="addSum">click me + 1</button>
+    </div>
+</template>
+
+<script lang="ts" setup name="Person">
+    import {ref, reactive} from 'vue'
+
+    let car = ref({
+        brand: 'bmw',
+        price: 100
+    })
+
+    let sum = ref(0)
+
+    function chgPrice() {
+        car.value.price += 10
+    }
+
+    function chgCar() {
+        // car = {brand:'benz', price:10} ** no change
+        // car = reactive({brand:'benz', price:10})  ** no change
+        // Object.assign(car, {brand: 'benz', price: 10}) for reactive
+        car.value = {brand: 'benz', price: 10}
+    }
+
+    function addSum() {
+        sum.value += 1
+    }
+
+</script>
+
+<style scoped>
+    .person {
+        background-color: skyblue;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,39 +1,27 @@
 <template>
     <div class="person">
-        <h2>A {{ car.brand }} car, price is {{ car.price }}</h2>
-        <button @click="chgPrice">change car price</button>
-        <button @click="chgCar">change car</button>
-        <hr>
-        <h2>sum: {{ sum }}</h2>
-        <button @click="addSum">click me + 1</button>
+        <h2>name : {{ person.name }}</h2>
+        <h2>age : {{ person.age }}</h2>
+        <button @click="chgName">change name</button>
+        <button @click="chgAge">change age</button>
     </div>
 </template>
 
 <script lang="ts" setup name="Person">
-    import {ref, reactive} from 'vue'
+    import {ref} from 'vue'
 
-    let car = ref({
-        brand: 'bmw',
-        price: 100
+    let person = ref({
+        name: 'bob',
+        age: 20
     })
 
-    let sum = ref(0)
-
-    function chgPrice() {
-        car.value.price += 10
+    function chgName() {
+        person.value.name += '~'
     }
 
-    function chgCar() {
-        // car = {brand:'benz', price:10} ** no change
-        // car = reactive({brand:'benz', price:10})  ** no change
-        // Object.assign(car, {brand: 'benz', price: 10}) for reactive
-        car.value = {brand: 'benz', price: 10}
+    function chgAge() {
+        person.value.age += 1
     }
-
-    function addSum() {
-        sum.value += 1
-    }
-
 </script>
 
 <style scoped>
